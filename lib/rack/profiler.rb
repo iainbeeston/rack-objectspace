20ea1ab453f1a7c3a2324578ecb6b9a96dbe7d68
Iain Beeston <iain.beeston@gmail.com>
2015-06-26 11:25:31 +0100

    Generated the gem using bundler

---
+++ lib/rack/profiler.rb
@@ -0,0 +1,7 @@
+require "rack/profiler/version"
+
+module Rack
+  module Profiler
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/profiler/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Profiler
+    VERSION = "0.1.0"
+  end
+end
